659d65ba39cc870939f1127a4d890bf3fde6c2d2
gitsava <asepdevops@gmail.com>
2021-12-06 19:46:25 +0700

    Start Origin From Referensi

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# mvc-fastapi
+# MVC-Fastapi API
 FastApi CRUD MYSQL with concept MVC
+
+#### 1. Saya menggunakan Anaconda
+#### 2. Buat lingkugan kerja (environment) Anaconda
+
+Di direktori repo utama
+```shell
+    conda env create -f envs/environment.yml
+    conda activate fastapi
+    pip freeze > envs/requirements.txt
+
+```
+
+## Menginstal dengan semua dependensi pada FastApi
+```shell
+pip install "fastapi[all]"
+```
+
+## Menginstall dependensi secara terpisah
+Lihat Referensi<a href=https://afrizalmy.com/tutorial-fastapi-crud-dengan-mysql>Click</a> disini
+```
+pip install fastapi
+pip install "uvicorn[standard]"
+pip install alembic
+pip install SQLAlchemy
+pip install pymsql
+pip install Faker
---
+++ migrations/versions/55ca3934ac00_create_table_pegawai.py
@@ -0,0 +1,42 @@
+"""create table pegawai
+
+Revision ID: 55ca3934ac00
+Revises: 
+Create Date: 2021-12-06 18:57:49.312948
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '55ca3934ac00'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from faker import Faker
+faker = Faker('id_ID')
+
+def upgrade():
+    pgw = op.create_table('dt_pegawai',
+        sa.Column('id_pegawai', sa.Integer, primary_key=True),
+        sa.Column('nama_pegawai', sa.String(255), nullable=False),
+        sa.Column('alamat_pegawai', sa.String(255)),
+        sa.Column('ttl_pegawai', sa.Date()),
+        sa.Column('telp_pegawai', sa.String(255)),
+        sa.Column('email_pegawai', sa.String(255), unique=True)
+    )
+    op.bulk_insert(
+        pgw,
+        [{
+            'nama_pegawai':faker.name(), 
+            'alamat_pegawai':faker.address(),
+            'ttl_pegawai':faker.date_of_birth(tzinfo=None, minimum_age=21, maximum_age=28),
+            'telp_pegawai':faker.phone_number(),
+            'email_pegawai':faker.email()
+        }for x in range(800)]
+    )
+
+def downgrade():
+    op.drop_table('dt_pegawai')
